77bc74d522c9957fd40692b74384171a892816be
Johan Levin <johan13@gmail.com>
2021-12-07 23:42:28 +0100

    Optimize day 7

--- src/day07/day07.ts
+++ src/day07/day07.ts
@@ -8,11 +8,23 @@ export function part1(filePath: string) {
 }
 
 export function part2(filePath: string) {
-    const input = fileParser(Number, ",")(filePath);
-    const fuelCost = (a: number, b: number) => {
-        const d = Math.abs(a - b);
-        return (d * (d + 1)) / 2;
-    };
-    const totalCost = (a: number) => R.sum(input.map(b => fuelCost(a, b)));
-    return Math.min(...R.range(Math.min(...input), Math.max(...input)+1).map(totalCost));
+    const input = fileParser(Number, ",")(filePath).sort(R.subtract);
+    const map = new Map(R.groupWith(R.equals, input).map(x => [x[0], x.length]));
+    const crabsPerPos = R.range(input[0], input[input.length - 1] + 1).map(x => map.get(x) ?? 0);
+    const fuelMovingRight = getFuelMovingRight(crabsPerPos);
+    const fuelMovingLeft = R.reverse(getFuelMovingRight(R.reverse(crabsPerPos)));
+    return Math.min(...R.zipWith((a, b) => a + b, fuelMovingRight, fuelMovingLeft));
+}
+
+function getFuelMovingRight(crabsPerPos: number[]) {
+    return R.scan(
+        (prev, numAtPos) => {
+            const numToLeft = prev.numToLeft + prev.numAtPos;
+            const fuelFromPrev = prev.fuelFromPrev + numToLeft;
+            const accumFuel = prev.accumFuel + fuelFromPrev;
+            return { numAtPos, numToLeft, fuelFromPrev, accumFuel };
+        },
+        { numAtPos: crabsPerPos[0], numToLeft: 0, fuelFromPrev: 0, accumFuel: 0 },
+        crabsPerPos.slice(1),
+    ).map(x => x.accumFuel);
 }
